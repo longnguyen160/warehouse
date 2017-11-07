de07a5180e6c2ae667d5d2f09f8393f736acf0c0
bimbeonguyen <longnguyen160@gmail.com>
2017-11-07 14:09:21 +0700

    stock

--- client/modules/stockOut/containers/StockOut.js
+++ client/modules/stockOut/containers/StockOut.js
@@ -4,41 +4,39 @@ import moment from 'moment';
 
 export const composer = ({ context, clearErrors }, onData) => {
   const { LocalState, Collections } = context();
-  let item = LocalState.get('ITEM');
+  let item = LocalState.get('ITEM') || '';
   let quantity = LocalState.get('QUANTITY');
   let itemData = null, boxes = [], shelves = [], blocks = [], sections = [], warehouses = [], actions = [];
 
-  if (item) {
-    const filter = {
-      name: item
-    };
+  const filter = {
+    name: item
+  };
 
-    if (Meteor.subscribe('getItem', filter).ready()) {
-      itemData = Collections.Items.findOne();
-      itemData.details = itemData.details.filter(detail => detail.warehouseId === Meteor.user().warehouseId)[0];
-      boxes = Collections.Boxes.find({ _id: { $in: itemData.details.boxId } }, { sort: { currentQuantity: 1 } }).fetch();
-      const shelfIds = boxes.map(box => box.map(element => element.shelfId));
-      shelves = shelfIds.map(shelfId =>
-        shelfId.map(element =>
-          Collections.Shelves.findOne({ _id: element })
-        )
-      );
-      blocks = Collections.Blocks.find({ _id: itemData.details.blockId }).fetch();
-      sections = Collections.Sections.find({ _id: itemData.details.sectionId }).fetch();
-      warehouses = Collections.Warehouses.find({ _id: itemData.details.warehouseId }).fetch();
-      if (quantity < itemData.details.quantity) {
-        let remainQuantity = quantity;
-        boxes = boxes.filter(box => {
-          if (remainQuantity > 0 && remainQuantity > box.currentQuantity) {
-            remainQuantity -= box.currentQuantity;
-            return box;
-          } else if (remainQuantity > 0 && remainQuantity < box.currentQuantity) {
-            box.currentQuantity -= remainQuantity;
-            remainQuantity = 0;
-            return box;
-          }
-        });
-      }
+  if (Meteor.subscribe('getItem', filter).ready()) {
+    itemData = Collections.Items.findOne();
+    itemData.details = itemData.details.filter(detail => detail.warehouseId === Meteor.user().warehouseId)[0];
+    boxes = Collections.Boxes.find({ _id: { $in: itemData.details.boxId } }, { sort: { currentQuantity: 1 } }).fetch();
+    const shelfIds = boxes.map(box => box.map(element => element.shelfId));
+    shelves = shelfIds.map(shelfId =>
+      shelfId.map(element =>
+        Collections.Shelves.findOne({ _id: element })
+      )
+    );
+    blocks = Collections.Blocks.find({ _id: itemData.details.blockId }).fetch();
+    sections = Collections.Sections.find({ _id: itemData.details.sectionId }).fetch();
+    warehouses = Collections.Warehouses.find({ _id: itemData.details.warehouseId }).fetch();
+    if (quantity < itemData.details.quantity) {
+      let remainQuantity = quantity;
+      boxes = boxes.filter(box => {
+        if (remainQuantity > 0 && remainQuantity > box.currentQuantity) {
+          remainQuantity -= box.currentQuantity;
+          return box;
+        } else if (remainQuantity > 0 && remainQuantity < box.currentQuantity) {
+          box.currentQuantity -= remainQuantity;
+          remainQuantity = 0;
+          return box;
+        }
+      });
     }
   }
 
